e6709b3ae1e20258495213daf337825a67a2fa11
Leonardo <Leonardo@Leonardo-PC.uptc.edu.co>
2014-06-10 10:58:30 -0500

    He creado la clase autor con sus atributos set y get para cada uno y dos contructores

---
+++ Karaoke/src/logica/Autor.java
@@ -0,0 +1,51 @@
+package logica;
+
+import java.util.ArrayList;
+
+public class Autor {
+
+	private String nombre;
+	private ArrayList<Cancion> listaCanciones;
+	private String foto;
+
+
+	public Autor(String nombre, ArrayList<Cancion> listaCanciones, String foto) {
+		super();
+		this.nombre = nombre;
+		this.listaCanciones = listaCanciones;
+		this.foto = foto;
+	}
+
+	public Autor() {
+		nombre = "";
+		listaCanciones = null;
+		foto = "";
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public ArrayList<Cancion> getListaCanciones() {
+		return listaCanciones;
+	}
+	
+	public void setListaCanciones(ArrayList<Cancion> listaCanciones) {
+		this.listaCanciones = listaCanciones;
+	}
+	
+	public String getFoto() {
+		return foto;
+	}
+	
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+
+
+}
