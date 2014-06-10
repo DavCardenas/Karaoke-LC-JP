8b1bf38282a2ab5f83ce5da721dd7e2f7a9e1e7e
llJuanPabloll <juancho.1910@hotmail.com>
2014-06-10 15:55:10 -0500

    Se documento la clase cancion

--- Karaoke/src/logica/Cancion.java
+++ Karaoke/src/logica/Cancion.java
@@ -1,22 +1,34 @@
 package logica;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
+import persistencia.ArchivoPlano;
+
 public class Cancion {
+	
+	/**
+	 *Description
+	 *
+	 * Esta clase permite traer el archivo cargado y mostrar su letra.
+	 */
 	
 	private String nombre;
 	private int duracion;
 	private ArrayList<String> letra;
 	private int numeroLineaActual;
 	private String Imagen;
+	private ArchivoPlano archivoPlano;
+	
 	
 	
 	public Cancion() {
 		nombre = null;
 		duracion = 0;
-		letra = new ArrayList<>();
+		letra = null;
 		numeroLineaActual = 0;
 		Imagen = null;
+		archivoPlano = new ArchivoPlano();
 		
 	}
 
@@ -73,6 +85,10 @@ public class Cancion {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 * Metodo que permite mostrar la letra de la cancion
+	 * @return cada linea de la letra.
+	 */
 	public String mostrarLetra() {
 		String linea = letra.get(numeroLineaActual);
 		numeroLineaActual+=1;
@@ -82,6 +98,17 @@ public class Cancion {
 	 return linea;
 	 	
 	}
+	/**
+	 * El metodo nos trae el archivo plano cargado en la persistencia, y lo almacenamos en el arrayList
+	 */
+	public void abrircancion(){
+		try {
+			letra = archivoPlano.abrirArchivo();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	
 
 }
