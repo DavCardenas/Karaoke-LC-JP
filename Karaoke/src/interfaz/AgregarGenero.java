55d37e623a3792998284595ff4a448d3847b98ce
Leonardo <Leonardo@Leonardo-PC.uptc.edu.co>
2014-06-13 13:38:57 -0500

    se han realizado varias clases aun sin terminar

---
+++ Karaoke/src/interfaz/AgregarGenero.java
@@ -0,0 +1,36 @@
+package interfaz;
+
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class AgregarGenero extends JDialog{
+
+	private JLabel jLabel;
+	private JTextField txtNombreGenero;
+	private JButton buttonAceptar;
+	
+	public final static String ACEPTAR = "ACEPTAR";
+	
+	public AgregarGenero(Karaoke karaoke, ManejadorDeEventos deEventos) {
+		
+		setSize(300,170);
+		setTitle("Agregar Genero");
+		setModal(true);
+		setLayout(new GridLayout(3, 1));
+		
+		jLabel = new JLabel("Ingrese el Genero");
+		txtNombreGenero = new JTextField();
+		buttonAceptar = new JButton("Aceptar");
+		buttonAceptar.setActionCommand(ACEPTAR);
+		buttonAceptar.addActionListener(deEventos);
+		
+		add(jLabel);
+		add(txtNombreGenero);
+		add(buttonAceptar);
+	}
+	
+}
--- Karaoke/src/interfaz/Karaoke.java
+++ Karaoke/src/interfaz/Karaoke.java
@@ -1,15 +1,31 @@
 package interfaz;
 
+import java.awt.BorderLayout;
+
 import javax.swing.JFrame;
+
+import logica.Genero;
 
 public class Karaoke extends JFrame{
 	
+	private PanelGeneros generos;
+	private logica.Karaoke karaoke;
 	public Karaoke() {
 		
 		setTitle("Karaoke LC-JP");
 		setSize(1000,500);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+		setLayout(new BorderLayout());
+		
+		generos = new PanelGeneros();
+		karaoke = new logica.Karaoke();
+		add(generos, BorderLayout.WEST);
+		
+	}
+	
+	public void actulizarListas() {
+		generos.actualizarLista(karaoke.getListaGeneros());
 	}
 
 }
