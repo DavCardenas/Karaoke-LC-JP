659a500c94869bd03b38a746d635b49c2edfc650
Leonardo <Leonardo@Leonardo-PC.uptc.edu.co>
2014-06-13 19:36:40 -0500

    Se termina de crear la clase Agregar Cancion de interfaz

--- Karaoke/src/interfaz/AgregarCancion.java
+++ Karaoke/src/interfaz/AgregarCancion.java
@@ -6,6 +6,7 @@ import java.awt.GridLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
@@ -22,8 +23,10 @@ public class AgregarCancion extends JDialog{
 	private JTextField txtDuracion;
 	private JButton buttonAceptar;
 	private JTextArea area;
+	private JLabel foto;
 	
 	public final static String ACEPTAR_CANCION = "ACEPTAR_CANCION";
+	public final static String SUBIR_IMAGEN = "SUBIR_IMAGEN";
 	
 	public AgregarCancion(KaraokePrincipal karaoke, ManejadorDeEventos deEventos) {
 		
@@ -55,13 +58,23 @@ public class AgregarCancion extends JDialog{
 		add(txtDuracion, gbc);
 		
 		area = new JTextArea();
-		gbc = new GridBagConstraints(0, 4, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 400, 200);
+		gbc = new GridBagConstraints(0, 4, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 400, 100);
 		add(area, gbc);
+		
+		foto = new JLabel(new ImageIcon(getClass().getResource("/Img/logo.png")));
+		gbc = new GridBagConstraints(0, 5, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0);
+		add(foto, gbc);
+		
+		cargarImagen = new JButton("Subir Imagen");
+		cargarImagen.setActionCommand(SUBIR_IMAGEN);
+		cargarImagen.addActionListener(deEventos);
+		gbc = new GridBagConstraints(0, 6, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0);
+		add(cargarImagen, gbc);
 		
 		buttonAceptar = new JButton("Aceptar");
 		buttonAceptar.setActionCommand(ACEPTAR_CANCION);
 		buttonAceptar.addActionListener(deEventos);
-		gbc = new GridBagConstraints(0, 5, 1, 1, 0, 0, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0);
+		gbc = new GridBagConstraints(0, 7, 1, 1, 0, 0, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, 5, 5, 5), 0, 0);
 		add(buttonAceptar, gbc);
 		
 	}
